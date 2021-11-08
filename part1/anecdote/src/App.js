d51be4b1d5ae0238abd393757dc986c299750fee
taurean04 <ayobamitomi@gmail.com>
2021-11-08 22:57:24 +0100

    step3

--- part1/anecdote/src/App.js
+++ part1/anecdote/src/App.js
@@ -1,5 +1,8 @@
 import React, { useState } from 'react';
 
+const Header = (props) => (<h1>{props.text}</h1>);
+const Button = ({handleClick, text}) => (<button onClick={handleClick}>{text}</button>);
+
 const App = () => {
   const anecdotes = [
     'If it hurts, do it more often',
@@ -9,25 +12,31 @@ const App = () => {
     'Premature optimization is the root of all evil.',
     'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
     'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients'
-  ]
+  ];
 
-  const [selected, setSelected] = useState(0)
-  const [vote, setVote] = useState(Array(anecdotes.length).fill(0))
+  const [selected, setSelected] = useState(0);
+  const [vote, setVote] = useState(Array(anecdotes.length).fill(0));
   const handleClick = () => {
     setSelected(Math.floor(Math.random() * anecdotes.length));
-  }
+  };
   const handleVote = () => {
     let votes = [...vote];
     votes[selected] += 1;
     setVote(votes);
-  }
+  };
+  const most_votes = anecdotes[vote.indexOf(Math.max(...vote))];
 
   return (
     <div>
+      <Header text='Anecdote of the day' />
       {anecdotes[selected]}
       <p>has {vote[selected]} votes</p>
-      <p><button onClick={handleVote}>vote</button></p>
-      <p><button onClick={handleClick}>next anecdote</button></p>
+      <p>
+        <Button handleClick={handleVote} text='vote'/>
+        <Button handleClick={handleClick} text='next anecdote' />
+      </p>
+      <Header text='Anecdote with most votes' />
+      <p>{most_votes}</p>
     </div>
   )
 }
