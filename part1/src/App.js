b8a8269be4b20053fd5d62bd1b12727f139818b9
taurean04 <ayobamitomi@gmail.com>
2021-11-08 09:46:45 +0100

    step2

--- part1/src/App.js
+++ part1/src/App.js
@@ -6,38 +6,48 @@ const Header = (props) => {
 }
 
 const Total = (props) => {
-  const total = props.e1 + props.e2 + props.e3;
+  let total = 0;
+  props.content.forEach(c => {
+    total += c.exercises
+  })
   return (<p>Number of exercises {total}</p>)
+}
+
+const Part = (props) => {
+  return(<p>{props.p} {props.e}</p>)
 }
 
 const Content = (props) => {
   const rendered = []
-  props.content.map((content, i) => {
-    rendered.push(<p key={i}>{content.p} {content.e}</p>)
+  props.content.forEach((content, i) => {
+    rendered.push(<Part key={i} p={content.part} e={content.exercises}/>)
   })
   return(<>{rendered}</>)
 }
 
 const App = () => {
   const course = 'Half Stack application development';
-  const part1 = 'Fundamentals of React';
-  const exercises1 = 10;
-  const part2 = 'Using props to pass data';
-  const exercises2 = 7;
-  const part3 = 'State of a component';
-  const exercises3 = 14;
 
   const content = [
-    {p: part1, e: exercises1},
-    {p: part2, e: exercises2},
-    {p: part3, e: exercises3}
+    {
+      part: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      part: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      part: 'State of a component',
+      exercises: 14
+    }
   ]
 
   return (
     <div>
       <Header course={course} />
       <Content content={content} />
-      <Total e1={exercises1} e2={exercises2} e3={exercises3}/>
+      <Total content={content}/>
     </div>
   )
 }
