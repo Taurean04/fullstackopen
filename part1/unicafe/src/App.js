18a286f0db86564749d6c20821805207cd7c0c4c
taurean04 <ayobamitomi@gmail.com>
2021-11-08 20:26:07 +0100

    unicafe step1

---
+++ part1/unicafe/src/App.js
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+
+const Header = (props) => (<h1>{props.text}</h1>)
+const Button = ({handleClick, text}) => (<button onClick={handleClick}>{text}</button>);
+const Paragraph = ({text, value}) => (<p>{text} {value}</p>);
+
+const App = () => {
+  // save clicks of each button to its own statement
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const incrementState = (setState, value) => {
+    setState(value);
+  }
+
+  return (
+    <div>
+      <Header text='give feedback' />
+      <Button handleClick={() => incrementState(setGood, good + 1)} text='good' />
+      <Button handleClick={() => incrementState(setNeutral, neutral + 1)} text='neutral' />
+      <Button handleClick={() => incrementState(setBad, bad + 1)} text='bad' />
+      <Header text='statistics' />
+      <Paragraph text='good' value={good} />
+      <Paragraph text='neutral' value={neutral} />
+      <Paragraph text='bad' value={bad} />
+    </div>
+  )
+}
+
+export default App;
