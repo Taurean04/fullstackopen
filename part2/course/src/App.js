4242128aa54b0f9b3885ca9e78eddcc568aa52e5
ositachiemeke <77016103+ositachiemeke@users.noreply.github.com>
2021-11-20 10:15:57 +0100

    seperate module

--- part2/course/src/App.js
+++ part2/course/src/App.js
@@ -1,28 +1,5 @@
-import React from 'react'
-
-
-const Header = ({name}) => (<h2>{name}</h2>)
-
-const Total = ({parts}) => {
-  const total = parts.reduce((p, c) => p + c.exercises, 0);
-  return (<p><strong>Total of {total} exercises</strong></p>);
-}
-
-const Part = ({name, exercise}) => (<p>{name} {exercise}</p>)
-
-const Content = ({content}) => (
-  <>
-    {content.map(item => <Part key={item.id} name={item.name} exercise={item.exercises} />)}
-  </>
-)
-
-const Course = ({course}) => (
-  <>
-    <Header name={course.name} />
-    <Content content={course.parts} />
-    <Total parts={course.parts} />
-  </>
-)
+import React from 'react';
+import Course from './components/Course';
 
 const App = () => {
   const courses = [
