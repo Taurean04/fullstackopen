585e99d0843de1be855d7e31062d9e4d3f64d50a
ositachiemeke <77016103+ositachiemeke@users.noreply.github.com>
2021-11-30 16:25:05 +0100

    step3

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -1,14 +1,27 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
+const Weather = ({weather, capital}) => {
+  return(
+    <>
+      <h3>Weather in {capital}</h3>
+      <p><strong>Temperature:</strong> {weather.current.temp_c} Celsius</p>
+      <img src={'https:' + weather.current.condition.icon} alt={capital}></img>
+      <p><strong>Wind:</strong> {weather.current.wind_mph} direction {weather.current.wind_dir}</p>
+    </>
+  )
+  
+}
+
 const Country = ({country}) => (
   <>
     <h2>{country.name.common}</h2>
-    <p>Capital: {country.capital[0]}</p>
+    {country.capital ? <p>Capital: {country.capital[0]}</p> : null}
     <p>Population: {country.population}</p>
     <h3>Languages</h3>
     <ul>{Object.values(country.languages).map((language, i) => <li key={i}>{language}</li>)}</ul>
     <img src={country.flags.png} alt={country.name.common}></img>
+    {country.weather ? <Weather weather={country.weather} capital={country.capital[0]} /> : null}
   </>
 );
 
@@ -21,15 +34,15 @@ const Result = ({countries, handleClick, show}) => {
   }else if(countries.length <= 10){
     result = <ul>{
       countries.map((country, i) => {
-        return <>
-        <li key={i}>
-          {country.name.common}
-          <button onClick={() => handleClick(i)}>
-            Show
-          </button>
-          {show[i] ? <Country key={country.name.common} country={country} /> : null}
-        </li>
-        </>
+        return (
+          <li key={i}>
+            {country.name.common}
+            <button onClick={() => handleClick(i)}>
+              Show
+            </button>
+            {show[i] ? <Country country={country} /> : null}
+          </li>
+        )
       })
     }</ul>;
   }
@@ -41,18 +54,36 @@ const App = () => {
   const [ countries, setCountries ] = useState([]);
   const [ search, setSearch ] = useState('');
 
+  const getCountries = res => {
+    res.data.map(data => {
+      let fails = ['Ngerulmud', 'Fakaofo', 'Naypyidaw', 'Kyiv'];
+      if(data.capital && data.capital[0] !== undefined && !fails.includes(data.capital[0])) {
+        if(data.capital[0] === 'Papeetē'){
+          data.capital[0] = 'Papeete';
+        }
+        if(data.capital[0] === 'Hagåtña'){
+          data.capital[0] = 'Hagatna';
+        }
+        axios.get(`http://api.weatherapi.com/v1/current.json?key=ff3e5df1c2dc4718a6f125917213011&q=${data.capital[0]}`)
+          .then(weather => {
+            if(weather.status === 400){
+              data.weather = null;                  
+            }
+            data.weather = weather.data;
+          });
+      }
+      return data;
+    });
+    setCountries(res.data);
+  }
+
   useEffect(() => {
     axios
       .get('https://restcountries.com/v3.1/all')
-      .then(res => {
-        setCountries(res.data);
-      });
+      .then(getCountries)
   }, []);
-  
-  let filtered = countries.filter(country => {
-    country.show = false
-    return country.name.common.toLowerCase().indexOf(search.toLowerCase()) !== -1;
-  });
+
+  let filtered = countries.filter(country => country.name.common.toLowerCase().indexOf(search.toLowerCase()) !== -1);
 
   const [ show, setShow ] = useState(Array(filtered.length).fill(false));
 
